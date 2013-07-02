6f3afecbfa870172239729c62c62cb071610f31f
Audrey Roy <audreyr@gmail.com>
2013-07-02 17:26:45 +0200

    More explicit, with better comments.

--- checkboxes/html_screenshot.py
+++ checkboxes/html_screenshot.py
@@ -16,18 +16,24 @@ import threading
 
 from ghost import Ghost
 
-def start_server():
-	""" Start up server to handle 1 request """
+def serve_one_request():
+	"""
+		Start up a simple web server serving files in the current directory, 
+		handle 1 request, and shut down again. 
+	"""
 	handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 	httpd = SocketServer.TCPServer(("", 8010), handler)
 	httpd.handle_request()
 
-t = threading.Thread(target=start_server)
+# Start webserver in new thread
+t = threading.Thread(target=serve_one_request)
 t.daemon = True
 t.start()
 
+# Use Ghost to open HTML page
 ghost = Ghost()
 page, extra_resources = ghost.open("http://127.0.0.1:8010/simple_form.html")
 
+# Capture a screenshot when the HTML page is ready
 ghost.wait_for_selector('button')
 ghost.capture_to('simple_form.png', zoom_factor=3.0)
